aba12f9b0ae37f6791c4ef5af9a1c346e69e1a94
shayan1234554321 <kshayan091@gmail.com>
2023-02-01 03:41:46 +0500

    created details popup html,css and js

--- script.js
+++ script.js
@@ -1,24 +1,130 @@
+// --------------------- Importing html elements --------------------
 const open = document.getElementById('open');
 const close = document.getElementById('close');
 const model = document.getElementById('model');
 const menuItem1 = document.getElementById('menuItem1');
 const menuItem2 = document.getElementById('menuItem2');
 const menuItem3 = document.getElementById('menuItem3');
+const closePopup = document.getElementById('closePopup');
+const popupArticle = document.getElementById('popupArticle');
+const popupContainer = document.getElementById('popupContainer');
+const title = document.getElementById('title');
+const description = document.getElementById('description');
+const techs = document.getElementById('techs');
+const image = document.getElementById('image');
+const liveVersion = document.getElementById('liveVersion');
+const source = document.getElementById('source');
+const worksContainer = document.getElementById('worksContainer');
 
-open.onclick = function () {
+// ------------------------ Works Array -------------------------------
+const works = [
+  {
+    title: 'The Light You Need',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: './images/workImage1.jpg',
+    techs: ['html', 'css', 'javascript'],
+    liveVersion: '#',
+    source: '#',
+  },
+  {
+    title: 'Sky-Rocket The World',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: './images/workImage2.jpg',
+    techs: ['html', 'css', 'javascript'],
+    liveVersion: '#',
+    source: '#',
+  },
+  {
+    title: 'Fresh Drinks',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: './images/workImage3.jpg',
+    techs: ['html', 'css', 'javascript'],
+    liveVersion: '#',
+    source: '#',
+  },
+  {
+    title: 'Customize-able Watches',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: './images/workImage4.jpg',
+    techs: ['html', 'css', 'javascript'],
+    liveVersion: '#',
+    source: '#',
+  },
+];
+
+// ------------------------ Functions -------------------------------
+
+function createWorkCard(Title, Description, Image, Techs, index) {
+  let techStacks = '';
+
+  Techs.forEach((tech) => {
+    techStacks += ` <li>${tech}</li>`;
+  });
+
+  const workHTML = document.createElement('article');
+  workHTML.className = 'article';
+  workHTML.innerHTML = `<img src='${Image}' alt="work Image" class="workImage">
+                        <div>
+                            <div class="workTitle">${Title}</div>
+                            <div class="attributes">
+                                <p class="atributeTitle">Details</p> 
+                                <ul>
+                                    <li>Backend Dev</li>
+                                    <li>2015</li>
+                                </ul> 
+                            </div>
+                            <p class="details" >${Description}</p>
+                            <div class="techStacks">
+                                <ul>
+                                  ${techStacks}
+                                </ul>
+                            </div>
+                            <button class="visitButton seeProject">See Project</button>
+                        </div>`;
+  workHTML.addEventListener('click', (() => {
+    techs.innerHTML = '';
+    popupArticle.style.display = 'flex';
+    title.innerHTML = works[index].title;
+    description.innerHTML = works[index].description;
+    image.src = works[index].image;
+    liveVersion.href = works[index].liveVersion;
+    source.href = works[index].source;
+    works[index].techs.forEach((tech) => {
+      const li = document.createElement('li');
+      li.innerHTML = tech;
+      techs.appendChild(li);
+    });
+  }));
+
+  worksContainer.appendChild(workHTML);
+}
+
+function modelDisplayNone() {
+  model.style.display = 'none';
+}
+
+function modelDisplayBlock() {
   model.style.display = 'block';
-};
+}
 
-close.onclick = function () {
-  model.style.display = 'none';
-};
+function popupDisplayNone() {
+  popupArticle.style.display = 'none';
+}
 
-menuItem1.onclick = function () {
-  model.style.display = 'none';
-};
-menuItem2.onclick = function () {
-  model.style.display = 'none';
-};
-menuItem3.onclick = function () {
-  model.style.display = 'none';
-};
+// ------------------------ Event listners -------------------------------
+
+window.addEventListener('load', (() => {
+  works.forEach((work, index) => {
+    createWorkCard(work.title, work.description, work.image, work.techs, index);
+  });
+}));
+popupContainer.addEventListener('click', ((e) => {
+  e.stopPropagation();
+}));
+menuItem1.addEventListener('click', (() => { modelDisplayNone(); }));
+menuItem2.addEventListener('click', (() => { modelDisplayNone(); }));
+menuItem3.addEventListener('click', (() => { modelDisplayNone(); }));
+close.addEventListener('click', (() => { modelDisplayNone(); }));
+open.addEventListener('click', (() => { modelDisplayBlock(); }));
+closePopup.addEventListener('click', (() => { popupDisplayNone(); }));
+popupArticle.addEventListener('click', (() => { popupDisplayNone(); }));
